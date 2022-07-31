4802680e9e30f0fda5bba8d84eeb5342acba3baf
Benny Christensen <bc@hp.cdbc>
2022-07-31 11:32:00 +0200

    Project skeleton created

---
+++ lfm_main.pas
@@ -0,0 +1,30 @@
+unit lfm_main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls;
+
+type
+
+  { TfrmMain }
+
+  TfrmMain = class(TForm)
+    sbaStatus: TStatusBar;
+  private
+
+  public
+
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
